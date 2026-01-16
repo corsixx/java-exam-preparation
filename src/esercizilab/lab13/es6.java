b97a042dc26c041e6ef76e8514c9218a9d60793b
corsixx <corso.davide05@gmail.com>
2026-01-16 18:45:26 +0100

    nuovo esercizio sulle stringhe

--- src/esercizilab/lab13/es6.java
+++ src/esercizilab/lab13/es6.java
@@ -1,5 +1,22 @@
 package esercizilab.lab13;
-
-public class es6 {
-
+import java.util.Scanner;
+/*Scrivere un programma Java che legga in input da tastiera una stringa e restituisca
+in output la stringa ottenuta trasformando la stringa in caratteri maiusco*/
+public class es6 
+{
+    static Scanner scanner = new Scanner(System.in);
+    static String trasformaStringa(String s1)
+    {
+        return s1.toUpperCase();
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println("TRASFORMAZIONE IN CARATTERI MAIUSCOLI");
+        String stringa1;
+        System.out.print("inserisci una stringa: ");
+        stringa1 = scanner.nextLine();
+        System.out.println();
+        stringa1 = trasformaStringa(stringa1);
+        System.out.println("stringa nuova: "+stringa1);
+    }
 }
