09b477f97aadcc45b2e3d567b8e5ea27f41b1525
corsixx <corso.davide05@gmail.com>
2026-01-18 15:33:33 +0100

    modifica del solver

--- src/eserciziesame/macchinettaDelCaffe/Solver.java
+++ src/eserciziesame/macchinettaDelCaffe/Solver.java
@@ -49,7 +49,6 @@ public class Solver {
                 }
                 case "SORT": {
                     macchinetta.ordinaProdottiPerCosto();
-
                     break;
                 }
                 case "F": {
