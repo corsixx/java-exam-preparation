e2e73d8be8bde2f5c77cf18c857998abce4e5b3b
corsixx <corso.davide05@gmail.com>
2026-01-18 20:06:24 +0100

    corretto per avere piu sicurezza

--- src/esercizilab/lab14/es2/TestComplex.java
+++ src/esercizilab/lab14/es2/TestComplex.java
@@ -7,9 +7,10 @@ public class TestComplex
     public static void main(String[] args) 
     {
         ArrayList<Complex> numeriComplessi = new ArrayList<>();
+        int volte =0;
         System.out.println("CLASSE PER TESTARE L'ABT COMPLEX");
         System.out.println("----------------------------------------------");
-        System.out.println("operazioni disponibili: aggiungi complesso, somma, sottrazione, moltiplicazione, modulo");
+        System.out.println("operazioni disponibili: aggiungicomplesso, somma, sottrazione, moltiplicazione, modulo");
         System.out.println("----------------------------------------------");
         System.out.print("scegli l'operazione da eseguire: ");
         String operazione = scanner.next();
@@ -18,7 +19,7 @@ public class TestComplex
         {
             switch(operazione)
             {
-                case "aggiungi complesso":
+                case "aggiungicomplesso":
                 {
                     System.out.print("inserisci parte reale: ");
                     double a = scanner.nextDouble();
@@ -26,78 +27,111 @@ public class TestComplex
                     double bi = scanner.nextDouble();
                     numeriComplessi.add(new Complex(a,bi));
                     System.out.println("numero complesso creato: "+numeriComplessi.get(numeriComplessi.size()-1).toString());
+                    volte++;
                     break;
                 }
                 case "somma":
                 {
-                    System.out.println("implementa somma");
-                    System.out.println("scegli i due numeri complessi da sommare (inserisci gli indici da 0): ");
-                    int index1 = scanner.nextInt();
-                    int index2 = scanner.nextInt();
-                    if(index1 < 0 || index1 >= numeriComplessi.size() || index2 < 0 || index2 >= numeriComplessi.size())
+                    if(volte<2)
                     {
-                        System.out.println("indici non validi");
+                        System.out.println("devi prima aggiungere almeno due numeri complessi");
                         break;
                     }
                     else
                     {
-                        System.out.println("i numeri che vuoi sommare sono: "+numeriComplessi.get(index1).toString()+" e "+numeriComplessi.get(index2).toString());
-                        System.out.println("il risultato è: "+numeriComplessi.get(index1).somma(numeriComplessi.get(index2)).toString());
+                        System.out.println("implementa somma");
+                        System.out.println("scegli i due numeri complessi da sommare (inserisci gli indici da 0): ");
+                        int index1 = scanner.nextInt();
+                        int index2 = scanner.nextInt();
+                        if(index1 < 0 || index1 >= numeriComplessi.size() || index2 < 0 || index2 >= numeriComplessi.size())
+                        {
+                            System.out.println("indici non validi");
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("i numeri che vuoi sommare sono: "+numeriComplessi.get(index1).toString()+" e "+numeriComplessi.get(index2).toString());
+                            System.out.println("il risultato è: "+numeriComplessi.get(index1).somma(numeriComplessi.get(index2)).toString());
+                        }
+                        break;
                     }
-                    break;
                 }   
                 case "sottrazione":
                 {
-                    System.out.println("implementa sottrazione");
-                    System.out.println("scegli i due numeri complessi da sottrarre (inserisci gli indici da 0): ");
-                    int index1 = scanner.nextInt();
-                    int index2 = scanner.nextInt();
-                    if(index1 < 0 || index1 >= numeriComplessi.size() || index2 < 0 || index2 >= numeriComplessi.size())
+                    if(volte<2)
                     {
-                        System.out.println("indici non validi");
+                        System.out.println("devi prima aggiungere almeno due numeri complessi");
                         break;
                     }
                     else
                     {
-                        System.out.println("i numeri che vuoi sottrarre sono: "+numeriComplessi.get(index1).toString()+" e "+numeriComplessi.get(index2).toString());
-                        System.out.println("il risultato è: "+numeriComplessi.get(index1).sottrazione(numeriComplessi.get(index2)).toString());
+                        System.out.println("implementa sottrazione");
+                        System.out.println("scegli i due numeri complessi da sottrarre (inserisci gli indici da 0): ");
+                        int index1 = scanner.nextInt();
+                        int index2 = scanner.nextInt();
+                        if(index1 < 0 || index1 >= numeriComplessi.size() || index2 < 0 || index2 >= numeriComplessi.size())
+                        {
+                            System.out.println("indici non validi");
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("i numeri che vuoi sottrarre sono: "+numeriComplessi.get(index1).toString()+" e "+numeriComplessi.get(index2).toString());
+                            System.out.println("il risultato è: "+numeriComplessi.get(index1).sottrazione(numeriComplessi.get(index2)).toString());
+                        }
+                        break;
                     }
-                    break;
                 }
                 case "moltiplicazione":
                 {
-                    System.out.println("implementa moltiplicazione");
-                    System.out.println("scegli i due numeri complessi da moltiplicare (inserisci gli indici da 0): ");
-                    int index1 = scanner.nextInt();
-                    int index2 = scanner.nextInt();
-                    if(index1 < 0 || index1 >= numeriComplessi.size() || index2 < 0 || index2 >= numeriComplessi.size())
+                    if(volte<2)
                     {
-                        System.out.println("indici non validi");            
+                        System.out.println("devi prima aggiungere almeno due numeri complessi");
                         break;
                     }
-                    else
+                    else    
                     {
-                        System.out.println("i numeri che vuoi moltiplicare sono: "+numeriComplessi.get(index1).toString()+" e "+numeriComplessi.get(index2).toString());
-                        System.out.println("il risultato è: "+numeriComplessi.get(index1).moltiplicazione(numeriComplessi.get(index2)).toString());
+                        System.out.println("implementa moltiplicazione");
+                        System.out.println("scegli i due numeri complessi da moltiplicare (inserisci gli indici da 0): ");
+                        int index1 = scanner.nextInt();
+                        int index2 = scanner.nextInt();
+                        if(index1 < 0 || index1 >= numeriComplessi.size() || index2 < 0 || index2 >= numeriComplessi.size())
+                        {
+                            System.out.println("indici non validi");            
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("i numeri che vuoi moltiplicare sono: "+numeriComplessi.get(index1).toString()+" e "+numeriComplessi.get(index2).toString());
+                            System.out.println("il risultato è: "+numeriComplessi.get(index1).moltiplicazione(numeriComplessi.get(index2)).toString());
+                        }
+                        break;
                     }
-                    break;
                 }
                 case "modulo":
                 {
-                    System.out.println("implementa modulo");
-                    System.out.println("scegli il numero complesso di cui vuoi calcolare il modulo (inserisci l'indice da 0): ");
-                    int index = scanner.nextInt();
-                    if(index < 0 || index >= numeriComplessi.size())    
+                    if(volte<1)
                     {
-                        System.out.println("indice non valido");
+                        System.out.println("devi prima aggiungere almeno un numero complesso");
                         break;
                     }
-                    else
+                    else    
                     {
-                        System.out.println("il numero complesso è: "+numeriComplessi.get(index).toString());
-                        System.out.println("il modulo è: "+numeriComplessi.get(index).Modulo());
+                        System.out.println("implementa modulo");
+                        System.out.println("scegli il numero complesso di cui vuoi calcolare il modulo (inserisci l'indice da 0): ");
+                        int index = scanner.nextInt();
+                        if(index < 0 || index >= numeriComplessi.size())    
+                        {
+                            System.out.println("indice non valido");
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("il numero complesso è: "+numeriComplessi.get(index).toString());
+                            System.out.println("il modulo è: "+numeriComplessi.get(index).Modulo());
+                        }
+                        break;
                     }
-                    break;
                 }
                 default:
                     System.out.println("operazione non riconosciuta");
