4bac243ff9fcbf83e7e799ee19f4b67dbfe2a18f
corsixx <corso.davide05@gmail.com>
2026-01-18 16:41:18 +0100

    aggiunte print di debug

--- src/eserciziteoria/esempioClassi/Solver.java
+++ src/eserciziteoria/esempioClassi/Solver.java
@@ -13,6 +13,9 @@ public class Solver
         people.add(p2);
         Group group = new Group("Marii", people);
         people = group.getPeople();
+        System.out.println("Numero di persone nel gruppo: " + people.size());
+        System.out.println("Primo nome della prima persona: " + people.get(0).getFirstName());
+        System.out.println("Primo nome della seconda persona: " + people.get(1).getFirstName());
         /*people è una variabile di tipo ArrayList, cioè un
         puntatore all’oggetto ArrayList (tipo ArrayList* in C) */
         people.clear();
@@ -20,6 +23,7 @@ public class Solver
         puntatore all’oggetto ArrayList (tipo ArrayList* in C)
         Le modifiche sono applicate sullo stesso oggetto, quindi anche
         su group.people */
-        System.out.println(group.getPeople().size());
+        System.out.println("clear eseguito, numero elementi :" + group.getPeople().size());
+        System.out.println("clear eseguito, numero elementi :" + people.size());
     }
 }
