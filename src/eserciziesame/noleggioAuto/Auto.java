1d462c747e04f8c50ebb4b494e59552162118d4f
corsixx <corso.davide05@gmail.com>
2026-01-19 15:15:09 +0100

    aggiunto auto + some Fixes

---
+++ src/eserciziesame/noleggioAuto/Auto.java
@@ -0,0 +1,18 @@
+package eserciziesame.noleggioAuto;
+
+/*Classe Auto: Sottoclasse di Veicolo le cui istanze rappresentano una au-
+to. Oltre alla targa, e l’anno di immatricolazione una auto è caratterizzata
+dall’attributo numeroPax che deve essere tra 2 e 8 e identifica il numero
+di persone per cui l’auto è immatricolata. Se il numero non è compreso tra
+2 e 8, viene assegnato di default il valore 5. */
+public class Auto extends Veicolo {
+    private int numeroPax;
+
+    public Auto(int numeroPax, String Targa, String Modello, int annoDiImmatricolazione, int numeroNoleggi) {
+        super(Targa, Modello, annoDiImmatricolazione, numeroNoleggi);
+        if (numeroPax >= 2 && numeroPax <= 8)
+            this.numeroPax = numeroPax;
+        else
+            this.numeroPax = 5;
+    }
+}
---
+++ src/eserciziesame/noleggioAuto/Moto.java
@@ -0,0 +1,13 @@
+package eserciziesame.noleggioAuto;
+
+public class Moto extends Veicolo {
+    private int Cavalli;
+
+    public Moto(int Cavalli, String Targa, String Modello, int annoDiImmatricolazione, int numeroNoleggi) {
+        super(Targa, Modello, annoDiImmatricolazione, numeroNoleggi);
+        if (Cavalli >= 200 && Cavalli <= 2000)
+            this.Cavalli = Cavalli;
+        else
+            this.Cavalli = 1000;
+    }
+}
