9b603254fc9f7478826cf98d25c0a6fe5dc4ab6b
corsixx <corso.davide05@gmail.com>
2026-01-19 14:16:14 +0100

    creata classe segmento

---
+++ src/esercizilab/lab14/es3/Segmento.java
@@ -0,0 +1,15 @@
+package esercizilab.lab14.es3;
+/*Progettare e implementare classi che usano classi definite in precedenza.
+Tempo: 30 min.
+Creare una classe Segmento basandosi su Vett. Per farlo, aggiungete i metodi
+necessari a Vett. Create i) un metodo equals per le classi Vett e per Segmento,
+che stabilisca se due oggetti – dello stesso tipo – sono equivalenti, e ii) un metodo
+copy, che crea una copia di un oggetto identico all’attuale. Fate lo stesso per la
+classe Cerchio.
+Testate i metodi sviluppati in un apposito main. */
+public class Segmento 
+{
+    private Vett puntoA;
+    private Vett PuntoB;
+    
+}
