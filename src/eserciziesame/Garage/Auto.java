275c4f09da2950ad7ae38e6b8c1d3a167d94aa99
corsixx <corso.davide05@gmail.com>
2026-01-20 14:26:12 +0100

    aggiunta classe Auto in Garage

---
+++ src/eserciziesame/Garage/Auto.java
@@ -0,0 +1,21 @@
+package eserciziesame.Garage;
+/*Classe Auto: rappresenta un'auto.
+• Attributi:
+	o String modello: il modello dell'auto.
+	o int targa: la targa dell'auto. E’ univoca per auto. Altrimenti detto, due auto devono avere targhe diverse.
+ */
+public class Auto 
+{
+    private String Modello;
+    private int Targa;
+    public Auto(String Modello, int Targa)
+    {
+        this.Modello = Modello;
+        this.Targa = Targa;
+    }
+    public Auto(Auto other)
+    {
+        this.Modello = other.Modello;
+        this.Targa = other.Targa;
+    }
+}
