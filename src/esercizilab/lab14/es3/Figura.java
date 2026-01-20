829b7e8e0ea876be647add242c941541242437bc
corsixx <corso.davide05@gmail.com>
2026-01-20 14:07:02 +0100

    aggiunta classe Figura

---
+++ src/esercizilab/lab14/es3/Figura.java
@@ -0,0 +1,8 @@
+package esercizilab.lab14.es3;
+/*Create una classe Figura fatta da 2 cerchi e tre segmenti. Verificare se Figura
+contiene cerchi concentrici tramite apposito main. Verificate anche se i tre segmenti
+costituiscono un triangolo con una funzione apposita. */
+public class Figura 
+{
+    
+}
